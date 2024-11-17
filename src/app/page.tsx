05d23f9c7658a489aef4027aff5559a5143d5e18
Guney Unay <guneymarketing17@hotmail.com>
2024-11-17 18:57:34 +0300

    feat: add new components and apply styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
+import { LatestPost } from '@/components/post'
+
+import { HydrateClient, api } from '@/trpc/server'
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.post.hello({ text: 'from tRPC' })
 
-  void api.post.getLatest.prefetch();
+  void api.post.getLatest.prefetch()
 
   return (
     <HydrateClient>
@@ -41,7 +42,7 @@ export default async function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+              {hello ? hello.greeting : 'Loading tRPC query...'}
             </p>
           </div>
 
@@ -49,5 +50,5 @@ export default async function Home() {
         </div>
       </main>
     </HydrateClient>
-  );
+  )
 }
