ac86dfaebc437f267212a4b60d537c1ea9203eb4
Guney Unay <guneymarketing17@hotmail.com>
2024-11-20 15:13:09 +0300

    chore: create a global config.ts for project settings

---
+++ src/app/config.ts
@@ -0,0 +1,12 @@
+/**
+ * This file contains configuration variables for the application.
+ * @file config.ts
+ * @description Defines constants and environment variables.
+ * @author Guney Unay
+ **/
+
+/**
+ * REDIRECT_TIME : This variable is used to set the time for the redirecting the user to the login page after the user logs out.
+ **/
+
+export const REDIRECT_TIME = 600
