255e5450edce5da84ea13051dbd544976ad96aa7
Guney Unay <guneymarketing17@hotmail.com>
2024-11-25 16:34:30 +0300

    bug: fixed minor bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 export function getBaseUrl() {
   if (typeof window !== 'undefined') return window.location.origin
-  if (process.env.VERCEL_URL) return `note-taking-web-app-tau.vercel.app`
+  if (process.env.VERCEL_URL)
+    return `https://note-taking-web-app-tau.vercel.app`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
